641cea431a7648df544714269ed1db5b0f728759
Haneen AbuKhater and Ryan Nunez <haneen@>
2017-07-10 11:08:39 -0700

    complete all requirements for the favorites webpage project

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $("#favorites").submit(function(event) {
+    var colorInput = $("input#color").val();
+    var foodInput = $("input#food").val();
+    var placeInput = $("input#place").val();
+    var inputs = [colorInput, foodInput, placeInput];
+    var newInputs = [];
+    newInputs.push(inputs[0], inputs[1], inputs[2]);
+    $(".inputs").text(inputs);
+    $("#inputResults").show();
+    $(".inputs2").text(newInputs);
+    $("#newArrayResults").show();
+    $("#item1").text(newInputs[0]);
+    $("#item2").text(newInputs[1]);
+    $("#item3").text(newInputs[2]);
+    $(".unorderList").show();
+    // document.write(
+    //   "<ul>"
+    //   + "<li>" + newInputs[0] + "</li>"
+    //   + "<li>" + newInputs[1] + "</li>"
+    //   + "<li>" + newInputs[2] + "</li>"
+    //   + "</ul>"
+    // );
+    event.preventDefault();
+ });
+});
